28cc464142664650afa199b551be6de15181b103
Aschalin <lukasz.bryniarski@gmail.com>
2018-05-30 16:42:40 +0200

    passed or failed and caesar finished

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,12 @@
-class CeasarCipher
-
+class CaesarCipher
+    def initialize(text, offset = 13)
+        @text = text.to_s
+        @alphabeth = Array('a'..'z').join + Array('A'..'Z').join
+        @cipher = (Array('a'..'z').rotate(offset) + Array('A'..'Z').rotate(offset)).join
+        
+    end
+    
+    def perform
+        puts "#{@text.tr(@alphabeth, @cipher)}"
+    end
 end
--- passed_or_failed.rb
+++ passed_or_failed.rb
@@ -1,3 +1,17 @@
 class PassedOrFailed
-
+    def initialize(hash, toPass)
+        
+        @passed = Hash.new
+        @failed = Hash.new
+        hash.each do |key, value|
+          if value.to_i >= toPass.to_i
+              @passed[key]=value
+          else
+              @failed[key]=value
+          end
+        end
+    end
+    def result
+        puts "#@passed, #@failed"
+    end
 end
